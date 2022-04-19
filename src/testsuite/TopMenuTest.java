4592c414c8a2620f426dc0af911b7849e7687dcf
hellojavagit <100705636+hellojavagit@users.noreply.github.com>
2022-04-19 22:56:47 +0100

    Update TopMenuTest.java

--- src/testsuite/TopMenuTest.java
+++ src/testsuite/TopMenuTest.java
@@ -17,67 +17,90 @@ public class TopMenuTest extends BaseTest {
         openBrowser(baseUrl);
     }
     @Test
-    public void userShouldNavigateToComputerPageSuccessfully(){
+    public void userShouldNavigateToComputerPageSuccessfully()
+    {
+      
+        //finding the computers element and clicking
+        WebElement computersElement = driver.findElement(By.xpath("//ul[@class='top-menu notmobile']//a[normalize-space()='Computers']"));
+        computersElement.click();
         //verifying the expected and actual messages by using xpath as locator
         String expectedMessage = "Computers";
-        String atualMessage = driver.findElement(By.xpath("//body/div[6]/div[2]/ul[1]/li[1]/a[1]")).getText();
-        Assert.assertEquals("Computers element does not match",expectedMessage,atualMessage);
-        driver.navigate().to("https://demo.nopcommerce.com/computers");
+        String actualMessage = driver.findElement(By.xpath("//body/div[6]/div[2]/ul[1]/li[1]/a[1]")).getText();
+        Assert.assertEquals("Computers element does not match", expectedMessage, actualMessage);
+    
 
     }
     @Test
-    public void userShouldNavigateToElectronicsPageSuccessfully()
-    {
+    public void userShouldNavigateToElectronicsPageSuccessfully() {
         String expectedMessage = "Electronics";
-        String atualMessage = driver.findElement(By.linkText("Electronics")).getText();
-        Assert.assertEquals("Electronics words does not match",expectedMessage,atualMessage);
-        driver.navigate().to("https://demo.nopcommerce.com/electronics");
+        //verifying the electronics tab and clicking
+        WebElement electronics = driver.findElement(By.linkText("Electronics"));
+        electronics.click();
+        //verifying the text Electronics
+        String actualMessage = driver.findElement(By.xpath("//h1[normalize-space()='Electronics']")).getText();
+        Assert.assertEquals("Electronics words does not match", expectedMessage, actualMessage);
     }
     @Test
     public void userShouldNavigateToApparelPageSuccessfully(){
+        //verifying the Apparel element tab and clicking
+        WebElement apparelElement = driver.findElement(By.xpath("//ul[@class='top-menu notmobile']//a[normalize-space()='Apparel']"));
+        apparelElement.click();
+        //verifying the text Apparel
         String expectedMessage = "Apparel";
-        String atualMessage = driver.findElement(By.xpath("//body/div[6]/div[2]/ul[1]/li[3]/a[1]")).getText();
-        Assert.assertEquals("Apparel element does not match",expectedMessage,atualMessage);
-        driver.navigate().to("http://demo.nopcommerce.com/Apparel");
-
-
+        String actualMessage = driver.findElement(By.xpath("//body/div[6]/div[2]/ul[1]/li[3]/a[1]")).getText();
+        Assert.assertEquals("Apparel element does not match",expectedMessage,actualMessage);
+       
     }
+
+    
 
     @Test
     public void userShouldNavigateToDigitalDownloadsPageSuccessfully(){
+        //clicking the digitaldownloads tab
+        WebElement digitalDownloads = driver.findElement(By.xpath("//ul[@class='top-menu notmobile']//a[normalize-space()='Digital downloads']"));
+        digitalDownloads.click();
+        //verifying the text Digital downloads
         String expectedMessage = "Digital downloads";
         String atualMessage = driver.findElement(By.xpath("//body/div[6]/div[2]/ul[1]/li[4]/a[1]")).getText();
         Assert.assertEquals("Digital Downloads element does not match",expectedMessage,atualMessage);
-        //navigating to digital downloads page
-        driver.navigate().to("https://demo.nopcommerce.com/digital-downloads");
+        
 
     }
     @Test
     public void userShouldNavigateToBooksPageSuccessfully(){
+         //finding the Books element and clicking
+        WebElement booksElement = driver.findElement(By.xpath("//ul[@class='top-menu notmobile']//a[normalize-space()='Books']"));
+        booksElement.click();
+        //verifying the text Books
         String expectedMessage = "Books";
-        String atualMessage = driver.findElement(By.xpath("//body/div[6]/div[2]/ul[1]/li[5]/a[1]")).getText();
-        Assert.assertEquals("Books element does not match",expectedMessage,atualMessage);
+        String actualMessage = driver.findElement(By.xpath("//body/div[6]/div[2]/ul[1]/li[5]/a[1]")).getText();
+        Assert.assertEquals("Books element does not match",expectedMessage,actualMessage);
         //navigating to Books page
-        driver.navigate().to("https://demo.nopcommerce.com/books");
+        //driver.navigate().to("https://demo.nopcommerce.com/books");
 
     }
     @Test
     public void userShouldNavigateToJewelryPageSuccessfully()
-    {
+    {   
+        //finding the jewellery tab present or not
+        WebElement jewelleryElement = driver.findElement(By.xpath("//ul[@class='top-menu notmobile']//a[normalize-space()='Jewelry']"));
+        jewelleryElement.click();
+        //verifying the text Jewelry
         String expectedMessage = "Jewelry";
-        String atualMessage = driver.findElement(By.xpath("//body/div[6]/div[2]/ul[1]/li[6]/a[1]")).getText();
-        Assert.assertEquals("Jewelry element does not match",expectedMessage,atualMessage);
-        //navigating to Jewelry page
-        driver.navigate().to("https://demo.nopcommerce.com/jewelry");
-
-    }
+        String actualMessage = driver.findElement(By.xpath("//body/div[6]/div[2]/ul[1]/li[6]/a[1]")).getText();
+        Assert.assertEquals("Jewelry element does not match",expectedMessage,actualMessage);
+       }
     @Test
     public void userShouldNavigateToGiftCardsPageSuccessfully() {
+        //clicking the Giftcards element on page
+        WebElement giftcardsElement = driver.findElement(By.xpath("//ul[@class='top-menu notmobile']//a[normalize-space()='Gift Cards']"));
+        giftcardsElement.click();
+        //verifying the text Gift Cards
         String expectedMessage = "Gift Cards";
-        String atualMessage = driver.findElement(By.xpath("//body/div[6]/div[2]/ul[1]/li[7]/a[1]")).getText();
-        Assert.assertEquals("Gift Cards element does not match", expectedMessage, atualMessage);
+        String actualMessage = driver.findElement(By.xpath("//body/div[6]/div[2]/ul[1]/li[7]/a[1]")).getText();
+        Assert.assertEquals("Gift Cards element does not match", expectedMessage, actualMessage);
         //navigates to giftcards page
-        driver.navigate().to("https://demo.nopcommerce.com/gift-cards");
+        //driver.navigate().to("https://demo.nopcommerce.com/gift-cards");
     }
     @After
     public void tearDown(){
